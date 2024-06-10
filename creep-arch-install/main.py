e6a91206dee9ee1ed7c93f9bd0cdfe6276d38dfb
Ricardo Arias <reariast128@proton.me>
2024-06-10 13:03:05 -0500

    Created desired partition schemes and methods to get partitions.

--- creep-arch-install/main.py
+++ creep-arch-install/main.py
@@ -1,10 +1,38 @@
 import os
+from partitions_operations import PartitionOperations as PartOps
+from partitions_schemes import PartitionSchemes
+
+def welcome() -> None:
+    print("Welcome to Creep's Arch Install Script.")
+
+def choice_partition_scheme() -> PartitionSchemes:
+    print("These are the supported partition schemes:")
+    print("\t( 1 ) Classic scheme: You will have a `/` ext4 partition, and a `/boot` fat32 partition.")
+    print("\t( 2 ) Separated home scheme: You will have a `/` ext4 partition, a `/home` partition, and a `/boot` partition.")
+
+    partition_choice = input("\nPlease, select between an option -> ")
+    while partition_choice not in ("1", "2"):
+        print("\nYou entered an invalid option.")
+        partition_choice = input("Please, select between an option -> ")
+
+    match int(partition_choice):
+        case 1:
+            return PartitionSchemes.CLASSIC_SCHEME
+        case 2:
+            return PartitionSchemes.SEPARATED_HOME_SCHEME
+
+
+def user_input_partition(partitions: PartOps.lsblk_output) -> str:
+    # TODO: Implement this.
+    
+    try:
+        desired_partition = input("Select an option -> ")
+
+    except Exception as e:
+        print("An error has ocurred:", e)
+
 
 if __name__ == "__main__":
-    print("Welcome to Creep's Arch Install Script.")
-    print("Showing all partitions...")
-    partitions = os.popen("lsblk")
-    line: str = " "
-    while line:
-        line = partitions.read()
-        print(line)
+    welcome()
+    desired_partition_scheme = choice_partition_scheme()
+    print(desired_partition_scheme.name)
