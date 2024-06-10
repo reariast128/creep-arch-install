492b89f0f7cfcea1e578acb178025af31e952f0a
Ricardo Arias <reariast128@proton.me>
2024-06-10 01:17:20 -0500

    Detected partitions.

--- creep-arch-install/main.py
+++ creep-arch-install/main.py
@@ -3,5 +3,8 @@ import os
 if __name__ == "__main__":
     print("Welcome to Creep's Arch Install Script.")
     print("Showing all partitions...")
-    partitions = os.popen("fdisk -l").readlines()
-    print(partitions)
+    partitions = os.popen("lsblk")
+    line: str = " "
+    while line:
+        line = partitions.read()
+        print(line)
