eed12832b720c6a58f10e154d0a6e7259987a6f3
Ricardo Arias <reariast128@proton.me>
2024-06-11 21:39:54 -0500

    Created update system clock method.

--- creep-arch-install/preparation.py
+++ creep-arch-install/preparation.py
@@ -1,4 +1,5 @@
 import socket
+import subprocess
 
 
 class Preparation:
@@ -29,6 +30,8 @@ class Preparation:
     def check_internet_connection(host: str = "9.9.9.9", port: int = 53, timeout: int = 3) -> bool:
         """Checks internet connection trying to connect to a DNS server."""
 
+        print(f"Trying to connect to {host}")
+
         try:
             socket.setdefaulttimeout(timeout)
             socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(
@@ -39,7 +42,16 @@ class Preparation:
             print(f"Cannot connect to internet: {ex}")
             return False
 
+    @staticmethod
+    def update_system_clock() -> None:
+        try:
+            subprocess.run(["timedatectl"])
+
+        except OSError as e:
+            print(f"Error while executing timedatectl: {e}")
+
 
 if __name__ == '__main__':
 
     print(Preparation.get_cpu_manufacturer())
+    Preparation.check_internet_connection()
